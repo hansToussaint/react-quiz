99a384a373c4d60ec322a25013b2c3f50b899458
hansToussaint <hanst605@gmail.com>
2023-09-14 12:48:47 +0300

    refactoring using context

--- src copy/components/Options.js
+++ src copy/components/Options.js
@@ -1,21 +1,19 @@
-import { useQuiz } from "../contexts/QuizContext";
-
-function Options({ question }) {
-  const { dispatch, answer } = useQuiz();
-
+function Options({ question, dispatch, answer }) {
   const hasAnswered = answer !== null;
 
   return (
     <div className="options">
       {question.options.map((option, index) => (
         <button
-          className={`btn btn-option ${index === answer ? "answer" : ""} ${
-            hasAnswered
-              ? index === question.correctOption
-                ? "correct"
-                : "wrong"
-              : ""
-          }`}
+          className={`btn btn-option ${index === answer ? "answer" : ""} 
+           ${
+             hasAnswered
+               ? index === question.correctOption
+                 ? "correct"
+                 : "wrong"
+               : ""
+           }
+          `}
           key={option}
           disabled={hasAnswered}
           onClick={() => dispatch({ type: "newAnswer", payload: index })}
--- src copy/components/StartScreen.js
+++ src copy/components/StartScreen.js
@@ -1,11 +1,7 @@
-import { useQuiz } from "../contexts/QuizContext";
-
-function StartScreen() {
-  const { numQuestions, dispatch } = useQuiz();
-
+function StartScreen({ numQuestions, dispatch }) {
   return (
     <div className="start">
-      <h2>Welcome to The React Quiz!</h2>
+      <h2>Welcome to the React Quiz</h2>
       <h3>{numQuestions} questions to test your React mastery</h3>
       <button
         className="btn btn-ui"
